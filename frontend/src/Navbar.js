67df051d782ee1d69b832a587662f0ffd1b041db
Trevor Wilson <wilsontp14@uww.edu>
2020-02-03 21:35:59 -0600

    Remove reference to unused modal component

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import LoginRegisterModal from './LoginRegisterModal';
 import Modal from 'react-modal';
 const axios = require('axios');
 const store = require('store');
